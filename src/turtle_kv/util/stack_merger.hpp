412fa5a817eb2e454cf77af69b61dc5b34458900
Tony Astolfi <tastolfi@gmail.com>
2026-05-07 13:16:43 -0400

    fix build

--- src/turtle_kv/util/stack_merger.hpp
+++ src/turtle_kv/util/stack_merger.hpp
@@ -1,8 +1,18 @@
+//=##=##=#==#=#==#===#+==#+==========+==+=+=+=+=+=++=+++=+++++=-++++=-+++++++++++
+//
+// Part of the TurtleKV Project, under Apache License v2.0.
+// See https://www.apache.org/licenses/LICENSE-2.0 for license information.
+// SPDX short identifier: Apache-2.0
+//
+//+++++++++++-+-+--+----- --- -- -  -  -   -
+
 #pragma once
+#define TURTLE_KV_UTIL_STACK_MERGER_HPP
 
 #include <turtle_kv/import/int_types.hpp>
 #include <turtle_kv/import/slice.hpp>
 
+#include <batteries/async/debug_info.hpp>
 #include <batteries/utility.hpp>
 
 #include <glog/logging.h>
