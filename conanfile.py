ff733b213742194a87fcea2fd604b56b8eeb72c1
Tony Astolfi <tastolfi@mathworks.com>
2026-08-12 16:25:55 -0400

    Fix for #30.

--- conanfile.py
+++ conanfile.py
@@ -92,7 +92,7 @@ class TurtleKvRecipe(ConanFile):
         self.requires("batteries/[>=0.71.1 <1]", **VISIBLE, **OVERRIDE)
         self.requires("boost/1.88.0", **VISIBLE, **OVERRIDE)
         self.requires("glog/0.7.1", **VISIBLE)
-        self.requires("llfs/[>=0.47.0 <1]", **VISIBLE)
+        self.requires("llfs/[>=0.48.0 <1]", **VISIBLE)
         self.requires("pcg-cpp/cci.20220409", **VISIBLE)
         self.requires("yaml-cpp/[>=0.9.0 <1]")
         self.requires("zlib/1.3.1", **OVERRIDE)
